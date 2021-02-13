d27aad3a34c31c968737ef2dbd7b7c5f18570e61
Erica Siegel <siegel.erica@gmail.com>
2021-02-13 02:07:33 -0800

    add front end files

--- server.js
+++ server.js
@@ -1,3 +1,26 @@
 // make sure to download dependency files:
-// npm install express sequelize mysql2
-// npm install dotenv -- make sure .env file is created 
+const path = require('path');
+
+const express = require('express');
+const routes = require('./routes');
+const sequelize = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+// turn on routes
+app.use(routes);
+
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
